4087a9ca86b2a34d7b3a05567d4cb6c3fee4e8f0
lucgus11 <lucas.gustin@indse.be>
2026-03-16 09:17:11 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,44 +1,49 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
   disable: process.env.NODE_ENV === "development",
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/image\.tmdb\.org\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "tmdb-images",
-        expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 * 30 },
+  workboxOptions: {
+    disableDevLogs: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/image\.tmdb\.org\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "tmdb-images",
+          expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 * 30 },
+        },
       },
-    },
-    {
-      urlPattern: /^https:\/\/api\.themoviedb\.org\/.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "tmdb-api",
-        expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 },
-        networkTimeoutSeconds: 10,
+      {
+        urlPattern: /^https:\/\/api\.themoviedb\.org\/.*/i,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "tmdb-api",
+          expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 },
+          networkTimeoutSeconds: 10,
+        },
       },
-    },
-    {
-      urlPattern: /\/_next\/static\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "next-static",
-        expiration: { maxEntries: 500, maxAgeSeconds: 60 * 60 * 24 * 365 },
+      {
+        urlPattern: /\/_next\/static\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "next-static",
+          expiration: { maxEntries: 500, maxAgeSeconds: 60 * 60 * 24 * 365 },
+        },
       },
-    },
-    {
-      urlPattern: /\/_next\/image\?.*/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "next-images",
-        expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 7 },
+      {
+        urlPattern: /\/_next\/image\?.*/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-images",
+          expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 7 },
+        },
       },
-    },
-  ],
+    ],
+  },
 });
 
 const nextConfig = {
